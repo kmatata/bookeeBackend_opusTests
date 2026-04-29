2f2860a78191e542ac35131f22131313d0902ab0
kmatata <254khoi@gmail.com>
2026-04-29 23:48:38 +0300

    add allow headers and expose headers for extension to be able to make requests to backend

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 
 from .api import router
@@ -8,6 +9,15 @@ from .lifespan import lifespan
 
 app = FastAPI(title="bookee-backend", lifespan=lifespan)
 app.include_router(router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+    expose_headers=["*"],
+)
 
 
 class _COEPMiddleware:
